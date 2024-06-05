a17cbc66e06225ed8ab31c5706329a7312ab31fe
rfauzan46 <saberfauzan46@gmail.com>
2024-06-06 06:09:09 +0700

    remove config from gitignore

---
+++ admin/config/theme/default.php
@@ -0,0 +1,20 @@
+<?php
+//============================================================+
+// File name   : default.php
+// Begin       : 2020-07-16
+// Last Update : 2023-11-30
+//
+// Description : Extra script to include when using the "default" theme.
+//
+// Author: Nicola Asuni
+//
+// (c) Copyright:
+//               Nicola Asuni
+//               Tecnick.com LTD
+//               www.tecnick.com
+//               info@tecnick.com
+//
+// License:
+//    Copyright (C) 2020-2024 Nicola Asuni - Tecnick.com LTD
+//    See LICENSE.TXT file for more information.
+//============================================================+
